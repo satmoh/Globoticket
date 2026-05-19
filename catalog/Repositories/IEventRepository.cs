87da62db842b84742f2c2d7ab9601960ba4cc83b
satmoh <mohamed.sanogo@eden-z.com>
2026-05-19 11:49:57 +0200

    Ajout d'un commentaire IEventRepository

--- catalog/Repositories/IEventRepository.cs
+++ catalog/Repositories/IEventRepository.cs
@@ -2,7 +2,10 @@ namespace GloboTicket.Catalog.Repositories;
 
 public interface IEventRepository
 {
-  Task<IEnumerable<Event>> GetEvents();
-  Task<Event> GetEventById(Guid eventId);
-  void UpdateSpecialOffer();
+    // return all events
+    Task<IEnumerable<Event>> GetEvents();
+    // return event by id
+    Task<Event> GetEventById(Guid eventId);
+    //set a special offer for an event
+    void UpdateSpecialOffer();
 }
