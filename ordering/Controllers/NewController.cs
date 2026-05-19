6378f8dafa8d1fc7ec3aa9fa5245f9faf06836b7
satmoh <mohamed.sanogo@eden-z.com>
2026-05-19 16:44:56 +0200

    feature: nouvelle fonctionalité

---
+++ ordering/Controllers/NewController.cs
@@ -0,0 +1,7 @@
+﻿namespace GloboTicket.Ordering.Controllers
+{
+    public class NewController
+    {
+        // This is a placeholder for the new controller. You can replace it with your actual actions and logic.
+    }
+}
---
+++ ordering/Services/NewService.cs
@@ -0,0 +1,7 @@
+﻿namespace GloboTicket.Ordering.Services
+{
+    public class NewService
+    {
+        // This is a placeholder for the new service. You can replace it with your actual methods and logic.
+    }
+}
