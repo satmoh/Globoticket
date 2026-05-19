6102b00d6cf20cec20e322aa18dc529dd28ba2a8
satmoh <mohamed.sanogo@eden-z.com>
2026-05-19 16:44:56 +0200

    feature: nouvelle fonctionalité

---
+++ ordering/Model/NewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GloboTicket.Ordering.Model
+{
+    public class NewModel
+    {
+        // This is a placeholder for the new model. You can replace it with your actual properties and methods.
+    }
+}
