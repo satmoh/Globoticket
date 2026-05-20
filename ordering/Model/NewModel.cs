7a3f9736419aee8b3f9fdb3bc307992928f95a8d
satmoh <mohamed.sanogo@eden-z.com>
2026-05-20 11:52:25 +0200

    Feature : Ajout de l'adresse mail

--- ordering/Model/NewModel.cs
+++ ordering/Model/NewModel.cs
@@ -6,5 +6,7 @@
         public Guid NewModelId { get; set; }
         public string? Name { get; set; }
         public string? Address { get; set; }
+        public string? Email { get; set; }
+
     }
 }
