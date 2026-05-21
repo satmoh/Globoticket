f266dc78e3ec9a5408d86fe767906e5dd01b01f8
satmoh <mohamed.sanogo@eden-z.com>
2026-05-21 15:06:29 +0200

    Feature : Prepare un conflit de code episode 2

--- ordering/Model/NewModel.cs
+++ ordering/Model/NewModel.cs
@@ -9,5 +9,7 @@
         public string? Email { get; set; }
         // another propertie
         public string? Password { get; set; }
+        // This is a placeholder to simulate a confilict code
+        //TODO: Simulate a conflict code
     }
 }
