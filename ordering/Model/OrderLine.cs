e71290d8401b61bec72f74c10fe34a181587779a
satmoh <mohamed.sanogo@eden-z.com>
2026-05-26 14:27:39 +0200

    Feature :  Raise a merge conflict episode 4

--- ordering/Model/OrderLine.cs
+++ ordering/Model/OrderLine.cs
@@ -5,4 +5,6 @@ public class OrderLine
     public Guid ConcertId { get; set; }
     public int TicketCount { get; set; }
     public int Price { get; set; }
+    // Feature Thirteen: this is a placeholder to raise a conflict
+    // tODO : Raise a conflict code for feature thirteen
 }
